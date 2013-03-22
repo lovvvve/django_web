9148093dd1023a50e514bade1f26cfb5cb843f6e
lovvvve <lovvvve@gmail.com>
2013-03-22 18:00:46 +0800

    first commit

---
+++ django_web/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, include, url
+from django_web.views import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'django_web.views.home', name='home'),
+    # url(r'^django_web/', include('django_web.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    (r'^time/$', current_datetime),
+    (r'^time/plus/(\d{1,2})/$', hours_ahead),
+)
