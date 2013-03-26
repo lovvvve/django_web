cdcb46382d5407141163d49f7697816120d36996
lovvvve <lovvvve@gmail.com>
2013-03-26 15:46:53 +0800

    修改连接的数据库!~~

--- django_web/settings.py
+++ django_web/settings.py
@@ -13,11 +13,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'test',                      # Or path to database file if using sqlite3.
+        'NAME': 'djangodb',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': '135230',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': 'lovvvve',
+        'PASSWORD': 'asd135230',
+        'HOST': '106.187.48.241',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
